d9a169674a916f967cbd2983ecfcd5c3e843b546
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-06-20 17:43:44 +0530

    models update

---
+++ CURD/CURD/Models/dataModels/UpsertEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace CURD.Models.dataModels
+{
+    public class UpsertEmployee
+    {
+        public int? EmpId { get; set; }
+        public string EmpName { get; set; } = null!;
+        public string EmpEmail { get; set; } = null!;
+        public long? EmpMobile { get; set; }
+        public long? EmpSalary { get; set; }
+
+        public int EmpDeptId { get; set; } 
+    }
+}
